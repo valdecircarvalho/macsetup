ac02a7ea31b15f783b1e7ac19898d543ddf6fdc8
homelaber <homelaber@gmail.com>
2019-04-28 15:58:14 -0300

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ set -x
 
 # download bootstrap files
 
-bash <(curl -s https://github.com/valdecircarvalho/macsetup/blob/master/install.sh -o $HOME/bootstrap/install.sh)
+curl -s https://github.com/valdecircarvalho/macsetup/blob/master/install.sh -o $HOME/bootstrap/install.sh
 
 chmod +x $HOME/bootstrap/*.sh
 
